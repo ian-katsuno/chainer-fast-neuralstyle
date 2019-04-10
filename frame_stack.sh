6e413bffbe4ad3ba781c41406443c1d55f011909
Ian Katsuno <ian.katsuno@gmail.com>
2019-04-10 11:20:38 -0400

    added styled to output paths

--- frame_stack.sh
+++ frame_stack.sh
@@ -8,6 +8,7 @@ echo "model path: $3"
 for f in $1/*.jpg
 	do
 		filename=$(basename $f)
+		filename="styled-$filename"
 		outputpath="$2/$filename"
 		echo "processing file $filename"
 		python generate.py $f -m $3 -o $outputpath -g 0
